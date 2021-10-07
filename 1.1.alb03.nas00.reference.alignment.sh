75165b8240a9413d7f81a711f58b9a802744aacc
Silu Wang <silu_wang@hotmail.com>
2021-10-07 14:12:08 -0700

    Update 1.1.alb03.nas00.reference.alignment.sh

--- 1.1.alb03.nas00.reference.alignment.sh
+++ 1.1.alb03.nas00.reference.alignment.sh
@@ -10,25 +10,9 @@
 #Alignment
 ref="/scratch/silu/abo.nas/abo.nas.hybrids/ref/kepul03FinalMaskedDrorep.fa"
 
-#1, DBMN21-B_S2_L007
-bwa mem $ref DBMN21-B_S2_L007_R1_001.fastq DBMN21-B_S2_L007_R2_001.fastq > sam/DBMN21-B_S2_L007.sam
-samtools view -b  sam/DBMN21-B_S2_L007.sam >  bam/DBMN21-B_S2_L007.bam
-
-#2, DBMN30-16_S49_L008
-bwa mem $ref DBMN30-16_S49_L008_R1_001.fastq DBMN30-16_S49_L008_R2_001.fastq > sam/DBMN30-16_S49_L008.sam
-samtools view -b  sam/DBMN30-16_S49_L008.sam >  bam/DBMN30-16_S49_L008.bam
-
-#3,DBMN30-19_S51_L008_R*_001.fastq
-bwa mem $ref DBMN30-19_S51_L008_R1_001.fastq DBMN30-19_S51_L008_R2_001.fastq > sam/DBMN30-19_S51_L008.sam
-samtools view -b  sam/DBMN30-19_S51_L008.sam >  bam/DBMN30-19_S51_L008.bam
-
-#4,DBCC035C4_S68_L008_R1_001.fastq.gz
-bwa mem $ref DBCC035C4_S68_L008_R1_001.fastq > sam/DBCC035C4_S68_L008.sam
-samtools view -b  sam/DBCC035C4_S68_L008.sam >  bam/DBCC035C4_S68_L008.bam
-
-$5, DBMN21-D_S4_L007_R*_001.fastq.gz
-bwa mem $ref DBMN21-D_S4_L007_R1_001.fastq DBMN21-D_S4_L007_R1_001.fastq > sam/DBMN21-D_S4_L007.sam
-samtools view -b  sam/DBMN21-D_S4_L007.sam >  bam/DBMN21-D_S4_L007.bam
+#align trimmed reads onto the reference for each parental individual
+#eg., DBMN21-B_S2_L007
+bwa mem $ref DBMN21-B_S2_L007_R1_001.fastq DBMN21-B_S2_L007_R2_001.fastq >  bam/DBMN21-B_S2_L007.bam
 
 
 
